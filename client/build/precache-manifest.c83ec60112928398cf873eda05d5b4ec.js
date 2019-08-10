5d6b2e4262e8cf06443565481e41e960135e8f64
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-08-10 12:32:08 -0400

    Initial commit

---
+++ client/build/precache-manifest.c83ec60112928398cf873eda05d5b4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b38eb3404b85f8038ab5",
+    "url": "/static/js/main.facda772.chunk.js"
+  },
+  {
+    "revision": "61477f81220081e1df57",
+    "url": "/static/js/2.901827b4.chunk.js"
+  },
+  {
+    "revision": "b38eb3404b85f8038ab5",
+    "url": "/static/css/main.83633c8f.chunk.css"
+  },
+  {
+    "revision": "b87f4d5b7a4d79c8d2b670a2f68ff45b",
+    "url": "/index.html"
+  }
+];
